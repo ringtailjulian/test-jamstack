958c2cf9765a93ab6b8602dadddda40f0739ffb8
YukiNakamuralian <contact.8u2i@gmail.com>
2022-07-21 14:12:53 +0900

    first commit

---
+++ pages/concept/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <layout-wrapper>
+    <layout-visual title="Concept" :height="40" visual="visual-concept" />
+    <div class="container w-full mx-auto pt-20 pb-20 px-6 md:px-0">
+      <h3 class="text-2xl text-red-800 font-extrabold mb-5 text-center">
+        打ち合わせやリモートワークができるちょっとした空間
+      </h3>
+      <div class="clearfix p-4 mb-10">
+        <img
+          src="~/assets/img/concept.jpg"
+          alt=""
+          class="float-right p-4 mb-10"
+        />
+        <div>
+          <p class="mb-5">
+            MdN Cafe
+            では、リモートワークや会社の打ち合わせなどが改定に行える空間を適用するカフェでwす<br />
+            リモートワークや打ち合わせを快適にするため席は完全個室にしています<br />
+            オンラインでの作業ができるようにwi-fi環境も完備しています。また、週末は勉強会の場所としても提供しています
+          </p>
+          <p class="mb-5">
+            お食事は天然素材を使ったドーナッツ・パンケーキを開発しています。<br />
+            シンプルな素材と調理方法で食材本来の味を引き立てる自慢のドーナッツ・パンケーキをご提供しています。<br />
+            コーヒーはブラジル産の豆を使用して、酸味とスッキリとして苦味があるコーヒーになります
+          </p>
+        </div>
+      </div>
+      <base-button>トップへ戻る</base-button>
+    </div>
+  </layout-wrapper>
+</template>
+<script>
+export default {
+  head() {
+    return {
+      title: 'コンセプト',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'コンセプトページの説明',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+.visual-home {
+  background-image: url('~@/assets/img/visual-home.jpg');
+}
+</style>
